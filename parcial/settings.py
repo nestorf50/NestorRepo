61988f571a33e8dac33b27cfcd5b02ac7cf5e168
GerAnt92 <brapema9@gmail.com>
2020-09-26 22:04:32 -0600

    Update databases

--- parcial/settings.py
+++ parcial/settings.py
@@ -77,19 +77,19 @@ WSGI_APPLICATION = 'parcial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'parcialnestor',
     }
-}'''
+}
 
 
 # Password validation
